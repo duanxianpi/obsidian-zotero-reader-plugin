a6fe65f57cf9318b7dd4b1530cb8f454212dd006
Xianpi Duan <duanxianpi@gmail.com>
2025-09-08 21:36:52 -0400

    adjust annotation callout block

--- src/view/annotation-manager.ts
+++ src/view/annotation-manager.ts
@@ -249,11 +249,17 @@ export class AnnotationManager {
 
 		// Comment (optional but we always include the block for stability)
 		const comment = json.comment || "";
-		pieces.push(
-			this.asBlockquote(
-				ensureTrailingNL(`${OzrpAnnoMarks.C_BEGIN} ${comment} ${OzrpAnnoMarks.C_END} ^${json.id}`)
-			)
-		);
+		// pieces.push("> " + OzrpAnnoMarks.C_BEGIN);
+		if (comment.trim() === "") {
+			pieces.push(this.asBlockquote(`${OzrpAnnoMarks.C_BEGIN} ${OzrpAnnoMarks.C_END} ^${json.id}`));
+		} else {
+			pieces.push(
+				this.asBlockquote(
+					ensureTrailingNL(`${OzrpAnnoMarks.C_BEGIN} ${comment}`)
+				)
+			);
+			pieces.push("> " + OzrpAnnoMarks.C_END + ` ^${json.id}`);
+		}
 		pieces.push("");
 
 		pieces.push(OzrpAnnoMarks.END);
